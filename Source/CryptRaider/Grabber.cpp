d32767b575fa5f9d2a29b1660d4c0f0ccbda4a65
Grégoire Lebas <gregoire.lebas@zerogamesstudios.com>
2024-08-13 20:43:37 +0200

    Added VS Tools and fixed the player blueprint.

--- Source/CryptRaider/Grabber.cpp
+++ Source/CryptRaider/Grabber.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Engine/World.h"
 #include "Grabber.h"
+#include "Engine/World.h"
 
 
 // Sets default values for this component's properties
